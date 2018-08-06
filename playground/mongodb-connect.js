6bd7c1efa25bafc70a24fa335a3a8d6d6888c586
Chillup <philipkaim@gmail.com>
2018-08-06 11:58:48 -0400

    connected to mongodb locally

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const { MongoClient, ObjectID } = require('mongodb');
+const Server = require('mongodb').Server
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }, (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to mongodb server.');
+    }
+    console.log('Connected to mongodb server.');
+    const db = client.db('TodoApp');
+
+    // db.collection('ToDos').insertOne({
+    //     title: 'something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+        
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'Philip',
+        age: 22,
+        location: 'Tampa, FL'
+    }, (err, result) => {
+        if (err) {
+            console.log('Unable to connect to mongodb server.', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+        
+    });
+
+    client.close();
+});
