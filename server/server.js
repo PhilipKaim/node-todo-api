34bc25e433d7934d783b2fc669aea3ccfa8ea26b
Chillup <philipkaim@gmail.com>
2018-08-12 13:04:08 -0400

    get todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('app listening on port 3000');
 });
