d622c9c06a457ecd1534dacb9de3a10313d7e7e3
Chillup <philipkaim@gmail.com>
2018-08-14 19:47:46 -0400

    DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-var port = process.env.PORT || 300;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -47,6 +47,24 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      res.status(200).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(404).send(e);
+  })
 });
 
 app.listen(port, () => {
