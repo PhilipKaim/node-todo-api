7114e1cf8937e3b3b13bef09b37cf6959dbf5541
Chillup <philipkaim@gmail.com>
2018-08-19 12:06:09 -0400

    POST users route and user password hashing

--- server/server.js
+++ server/server.js
@@ -94,6 +94,19 @@ app.patch('/todos/:id', (req, res) => {
   }).catch(err => res.send(400).send(err));
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
