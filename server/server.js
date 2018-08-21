64be82551b53b7775e2476e41a086d4710de4318
Chillup <philipkaim@gmail.com>
2018-08-21 18:47:03 -0400

    add GET users/me authenticate

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 var port = process.env.PORT;
@@ -105,6 +106,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 });
 
 app.listen(port, () => {
